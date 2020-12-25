4db8f2ecda52d70e450a50e4fe1c9dc3d6b05709
balamurugan213 <balainfo21@gmail.com>
2020-12-25 22:26:22 +0530

    firestore functions

--- README.md
+++ README.md
@@ -58,3 +58,70 @@ db.collection('Books').add({
 ```
 
 ---
+
+## deleting values in the database
+
+```javascript
+
+e.stopPropagation();
+let id =e.target.parentElement.getAttribute('data-id');
+db.collection('Books').doc(id).delete();
+
+```
+
+---
+
+## Query a particular data in the database
+
+Querying can be done using the where function and orderBy function for in order.
+
+```javascript
+
+db.collection('Books').where('type', '!=', 'essay').orderBy('type').get().then((snapshot)=>{
+    snapshot.forEach(doc =>{
+        console.log(doc.data());
+        renderList(doc)
+    })
+}).catch(function(error) {
+    console.log("Error getting documents: ", error);
+});
+
+```
+
+---
+
+## Instant change of data in the database
+
+onSnapshot method is used to listen the update in the database and respond respective to it.
+
+```javascript
+db.collection('Books').orderBy('catagory').onSnapshot(snapshot =>{
+    let changes=snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == 'added')
+        {
+            console.log(change)   
+            renderList(change.doc) 
+        }
+        else if(change.type == 'removed')
+        {
+            let li =bookList.querySelector('[data-id='+change.doc.id +']');
+            bookList.removeChild(li);
+        }
+       
+    });
+})
+```
+
+## Update of data in the database
+
+Update can be done by using set function update function.Set function overwrite the data
+
+```javascript
+db.collection('Books').doc(id).update({
+    name:'india'
+})
+db.collection('Books').doc(id).set({
+    name:'india'
+})
+```
--- app.js
+++ app.js
@@ -18,30 +18,68 @@ db.settings({timestampsInSnapshots: true});
 const bookList=document.querySelector('#cafe-list');
 const form =document.querySelector('#add-cafe-form');
 
-//Snapshoting the db
-db.collection('Books').get().then((snapshot)=>{
-    snapshot.forEach(doc =>{
-        console.log(doc.data());
-        renderList(doc)
-    })
+// //Snapshoting the db
+// db.collection('Books').where('catagory', '!=', 'essay').orderBy('catagory').get().then((snapshot)=>{
+//     snapshot.forEach(doc =>{
+//         console.log(doc.data());
+//         renderList(doc)
+//     })
+// }).catch(function(error) {
+//     console.log("Error getting documents: ", error);
+// });
+
+
+// Snapshoting the db
+db.collection('Books').orderBy('catagory').onSnapshot(snapshot =>{
+    let changes=snapshot.docChanges();
+    changes.forEach(change => {
+        if(change.type == 'added')
+        {
+            console.log(change)   
+            renderList(change.doc) 
+        }
+        else if(change.type == 'removed')
+        {
+            let li =bookList.querySelector('[data-id='+change.doc.id +']');
+            bookList.removeChild(li);
+        }
+       
+    });
 })
 
-// 
 
 // getting data
 function renderList(doc){
     let li = document.createElement('li');
     li.setAttribute('data-id',doc.id);
+
     let name = document.createElement('span');
     name.textContent=doc.data().name;
-    let type = document.createElement('span');
-    type.textContent=doc.data().type;
+
+    let catagory = document.createElement('span');
+    catagory.textContent=doc.data().catagory;
+
+    let cross=document.createElement('div');
+    cross.textContent="x";
+
 
     li.appendChild(name);
-    li.appendChild(type);
+    li.appendChild(catagory);
+    li.appendChild(cross);
+
+
     bookList.appendChild(li)
+
+    // adding functionality
+    cross.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        let id =e.target.parentElement.getAttribute('data-id');
+        db.collection('Books').doc(id).delete();
+    })
     
 }
+
+
 
 // adding data
 form.addEventListener('submit',(e)=>{
@@ -49,8 +87,8 @@ form.addEventListener('submit',(e)=>{
 
     db.collection('Books').add({
         name:form.name.value,
-        type:form.type.value
+        catagory:form.catagory.value
     })
     form.name.value=''
-    form.type.value=''
+    form.catagory.value=''
 })
